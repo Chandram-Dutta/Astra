1b5f52d8e49801ff5325d0cc768b92fd2e170c62
Chandram-Dutta <chandramdutta2004@gmail.com>
2023-12-05 04:27:53 +0530

    Initial Commit

---
+++ Astra/AstraApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstraApp.swift
+//  Astra
+//
+//  Created by Chandram Dutta on 05/12/23.
+//
+
+import SwiftUI
+
+@main
+struct AstraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Astra/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Astra
+//
+//  Created by Chandram Dutta on 05/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
